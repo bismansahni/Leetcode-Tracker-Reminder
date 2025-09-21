544e6a38a0609397180df9452df210aecc188732
bismansahni <bismansahni@outlook.com>
2025-09-21 01:02:36 -0700

    fix.

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,9 +4,9 @@ let redis: Redis | undefined;
 
 export function getRedis() {
     if (!redis) {
-        const url = process.env.SHIT_KV_REST_API_URL;
+        const url = process.env.UPSTASH_REDIS_REST_URL;
 
-        const token = process.env.SHIT_KV_REST_API_TOKEN;
+        const token = process.env.UPSTASH_REDIS_REST_TOKEN;
 
         if (!url || !token) {
             throw new Error(
